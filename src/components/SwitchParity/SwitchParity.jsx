4c0f734d5326390de17d7d09351113ee8f0047e7
ArtyomMir <amirosnicenko37@gmail.com>
2023-04-02 01:27:50 +0300

    Added current week button

--- src/components/SwitchParity/SwitchParity.jsx
+++ src/components/SwitchParity/SwitchParity.jsx
@@ -5,11 +5,24 @@ const SwitchParity = ({updateParity}) => {
     useEffect(() => {
         updateParity(checked);
     }, [checked]);
-    return (
+
+function isEven(){
+        const currentDate= new Date();
+        const semesterStartDate =  new Date(currentDate.getFullYear(),1,6);
+        const numberOfDays = Math.floor((currentDate - semesterStartDate) / (24 * 60 * 60 * 1000));
+        const numberOfWeeks = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
+        setChecked((!Boolean(numberOfWeeks % 2)));
+    }
+
+    return (<>
         <div className="switch">
             <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
-             {checked ? 'Чётная' : 'Нечётная'}
+             {checked ? 'Чётная' : 'Нечётная'} 
         </div>
+        <div className='parityButton'>
+        <button onClick={() => isEven()} >Показать текущую неделю</button>
+        </div>
+        </>
     );
 };
 
