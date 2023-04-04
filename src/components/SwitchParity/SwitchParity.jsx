b2244aba6dd410d994d2ec669c9842dd89930387
Quadrog101 <Quadrog101@gmail.com>
2023-04-04 13:41:03 +0300

    Fixes

--- src/components/SwitchParity/SwitchParity.jsx
+++ src/components/SwitchParity/SwitchParity.jsx
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const SwitchParity = ({updateParity}) => {
     const [checked, setChecked] = useState(false);
+
     useEffect(() => {
         updateParity(checked);
     }, [checked]);
+
+    function isEven(){
+        const currentDate= new Date();
+        const semesterStartDate =  new Date(currentDate.getFullYear(),1,6);
+        const numberOfDays = Math.floor((currentDate - semesterStartDate) / (24 * 60 * 60 * 1000));
+        const numberOfWeeks = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
+        setChecked((!Boolean(numberOfWeeks % 2)));
+    };
+
     return (
-        <div className="switch">
-            <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
-             {checked ? 'Нижняя неделя' : 'Верхняя неделя'}
+        <div>
+            <div className="switch">
+                <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
+                {checked ? 'Нижняя неделя' : 'Верхняя неделя'}
+            </div>
+            <div className='parityButton'>
+                <button onClick={() => isEven()} >Показать текущую неделю</button>
+            </div>
         </div>
     );
 };
