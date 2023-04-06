ef49cb3a291b2684dfee79e5ca024a3c72c1ee69
Quadrog101 <Quadrog101@gmail.com>
2023-04-06 09:28:17 +0300

    Function isEven moved to SwitchParity, changed semester start

--- src/components/ScheduleGrid/ScheduleGrid.jsx
+++ src/components/ScheduleGrid/ScheduleGrid.jsx
@@ -3,14 +3,6 @@ import {Timeline} from "../Timeline/Timeline";
 import {DayColumn} from "../DayColumn/DayColumn";
 import './schedule.scss';
 
-function isEven(){
-    const currentDate= new Date();
-    const semesterStartDate =  new Date(currentDate.getFullYear(),1,6);
-    const numberOfDays = Math.floor((currentDate - semesterStartDate) / (24 * 60 * 60 * 1000));
-    const numberOfWeeks = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
-    return !Boolean(numberOfWeeks % 2);
-}
-
 const ScheduleGrid = ({events,parity}) => {
     const days = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
     //const currentParity = isEven(); 
--- src/components/SwitchParity/SwitchParity.jsx
+++ src/components/SwitchParity/SwitchParity.jsx
@@ -9,14 +9,14 @@ const SwitchParity = ({updateParity}) => {
 
     function isEven(){
         const currentDate= new Date();
-        const semesterStartDate =  new Date(currentDate.getFullYear(),1,6);
+        const semesterStartDate =  new Date(currentDate.getFullYear(),1,7);
         const numberOfDays = Math.floor((currentDate - semesterStartDate) / (24 * 60 * 60 * 1000));
         const numberOfWeeks = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
         setChecked((!Boolean(numberOfWeeks % 2)));
     };
 
     return (
-        <div>
+        <div className='properties'>
             <div className="switch">
                 <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
                 {checked ? 'Нижняя неделя' : 'Верхняя неделя'}
